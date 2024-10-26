6fe74cfa0c63a176e05790bb843845b7e93795f5
priyanshu-9651 <152474251+priyanshu-9651@users.noreply.github.com>
2024-10-26 14:02:03 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,31 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//null ke liye comparison different trike se kam karta hai only (null >= 0) hi true deta hai baki pe false.
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//is case pe sare pe false deta hai.
+
+// === 
+
+console.log("2" === 2);
+
+//strict checking ho rhi hai.
+
+
+
+//finally basic comaprison pe koi problem nhi hai lekin  string or number ko comparsion pe problem ati hai.
